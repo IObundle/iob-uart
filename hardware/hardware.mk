50342dbde49fa42646e114a4a88f07ab323d6755
André Merendeira <andre.merendeira@gmail.com>
2021-02-25 15:50:48 +0000

    removed deprecated sw_reg_gen.vh file

--- hardware/hardware.mk
+++ hardware/hardware.mk
@@ -29,10 +29,9 @@ VSRC+=$(UART_HW_DIR)/src/uart_core.v $(UART_HW_DIR)/src/iob_uart.v
 $(UART_HW_DIR)/include/UARTsw_reg_gen.v: $(UART_HW_DIR)/include/UARTsw_reg.v
 	$(LIB_DIR)/software/mkregs.py $< HW
 	mv UARTsw_reg_gen.v $(UART_HW_DIR)/include
-	mv UARTsw_reg.vh $(UART_HW_DIR)/include
 
 uart_clean_hw:
-	@rm -rf $(UART_HW_DIR)/include/UARTsw_reg_gen.v $(UART_HW_DIR)/include/UARTsw_reg.vh tmp $(UART_HW_DIR)/fpga/vivado/XCKU $(UART_HW_DIR)/fpga/quartus/CYCLONEV-GT
+	@rm -rf $(UART_HW_DIR)/include/UARTsw_reg_gen.v tmp $(UART_HW_DIR)/fpga/vivado/XCKU $(UART_HW_DIR)/fpga/quartus/CYCLONEV-GT
 
 .PHONY: uart_clean_hw
 
