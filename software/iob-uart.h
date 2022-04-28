c9dabc35e7d1dfb507b32729547a35864fa97202
P-Miranda <pmiranda.iobundle@gmail.com>
2022-04-28 15:32:58 +0100

    feat(doc): Initial atempt at Doxygen sw drivers - Initial atempt to use Doxygen to generate software drivers for UART - run `make -C document/sw all` to generate documentation - check `document/sw/Doxyfile` for configurations

--- software/iob-uart.h
+++ software/iob-uart.h
@@ -1,24 +1,49 @@
+/**
+ * @file iob-uart.h
+ *
+ * @brief IOb-Uart software drivers.
+ *
+ * Public driver functions for the IOb-Uart peripheral.
+ */
 #include <stdlib.h>
 #include <stdarg.h>
 #include <stdint.h>
 
 #include "iob_uart_swreg.h"
 
+/**
+ * @def UART_PROGNAME
+ *
+ * Prefix to IOb-Uart specific prints.
+ */
 #define UART_PROGNAME "IOb-UART"
 
-//UART commands
-#define STX 2 //start text
-#define ETX 3 //end text
-#define EOT 4 //end of transission
-#define ENQ 5 //enquiry
-#define ACK 6 //acklowledge
-#define FTX 7 //transmit file
-#define FRX 8 //receive file
+/** 
+ * @defgroup UART Commands
+ *
+ * @brief Encoding for the UART commands.
+ *
+ * These command codes are used to signal commands to console program.
+ */
+#define STX 2 /// start text
+#define ETX 3 /// end text
+#define EOT 4 /// end of transission
+#define ENQ 5 /// enquiry
+#define ACK 6 /// acklowledge
+#define FTX 7 /// transmit file
+#define FRX 8 /// receive file
 
-
-//UART functions
-
-//Reset UART and set the division factor
+/** @brief Initialize UART.
+ *
+ * Reset UART, set IOb-Uart base address and set the division factor.
+ * The division factor is the number of clock cycles per simbol transfered.
+ * For example, for a case with fclk = 100 Mhz for a baudrate of 115200 we
+ * should have `div=(100*10^6/115200) = (868).
+ *
+ * @param base_address IOb-Uart instance base address in the system.
+ * @param div Equal to round (fclk/baudrate). 
+ * @return void.
+ */ 
 void uart_init(int base_address, uint16_t div);
 
 //Close transmission
