cb9a7dde362c1b9a4971a96f48710206c9beb073
P-Miranda <pmiranda.iobundle@gmail.com>
2022-05-02 16:56:26 +0100

    feat(doc): Initial version of UART SW drivers - Initial version of software drivers for IOb-UART - includes information about the MACROS and functions declared in   `software/iob-uart.h` - Check Doxygen documentation in `document/sw` - Generate new Doxygen documentation: ``` make -C document/sw/ all evince document/sw/*.pdf & ``` - Check `document/sw/Makefile` and `document/sw/Doxygen` for more   implementation details.

--- software/iob-uart.h
+++ software/iob-uart.h
@@ -19,17 +19,52 @@
 #define UART_PROGNAME "IOb-UART"
 
 /** 
- * @defgroup UART Commands
+ * @def STX
  *
- * @brief Encoding for the UART commands.
+ * Start text.
+ * Signal start of data sequence to be printed.
+ */
+/** 
+ * @def ETX
  *
- * These command codes are used to signal commands to console program.
+ * End text.
+ * Signal end of data sequence to be printed.
+ */
+/** 
+ * @def EOT
+ *
+ * End of transmission.
+ * Signal end of UART connection.
+ */
+/** 
+ * @def ENQ
+ *
+ * Enquiry.
+ * Signal start of UART connection.
+ */
+/** 
+ * @def ACK
+ *
+ * Acknowledge.
+ * Signal reception of incomming message.
+ */
+/** 
+ * @def FTX
+ *
+ * File transfer.
+ * Signal file transfer request.
+ */
+/** 
+ * @def FRX
+ *
+ * File reception.
+ * Signal file reception request.
  */
 #define STX 2 /// start text
 #define ETX 3 /// end text
 #define EOT 4 /// end of transission
 #define ENQ 5 /// enquiry
-#define ACK 6 /// acklowledge
+#define ACK 6 /// acknowledge
 #define FTX 7 /// transmit file
 #define FRX 8 /// receive file
 
@@ -37,8 +72,9 @@
  *
  * Reset UART, set IOb-Uart base address and set the division factor.
  * The division factor is the number of clock cycles per simbol transfered.
+ *
  * For example, for a case with fclk = 100 Mhz for a baudrate of 115200 we
- * should have `div=(100*10^6/115200) = (868).
+ * should have `div=(100*10^6/115200) = (868)`.
  *
  * @param base_address IOb-Uart instance base address in the system.
  * @param div Equal to round (fclk/baudrate). 
@@ -46,33 +82,89 @@
  */ 
 void uart_init(int base_address, uint16_t div);
 
-//Close transmission
+/** @brief Close transmission.
+ *
+ * Send end of transmission (EOT) command via UART.
+ * Active wait until TX transfer is complete.
+ * Use this function to close console program.
+ *
+ * @return void.
+ */ 
 void uart_finish();
 
-//TX FUNCTIONS
-
-//Enable / disable tx
-void uart_txen(uint8_t val);
-
-//Wait for tx to be ready
+/** @brief Wait for TX.
+ *
+ * Active wait until TX is ready to process new byte to send.
+ *
+ * @return void.
+ */ 
 void uart_txwait();
 
-//Print char
+/** @brief Print char.
+ *
+ * Send character via UART to be printed by in console program.
+ *
+ * @param c Character to print.
+ * @return void.
+ */ 
 void uart_putc(char c);
 
-//Print string
+/** @brief Print string.
+ *
+ * Send string via UART to be printed by in console program.
+ *
+ * @param s Pointer to char array to be printed.
+ * @return void.
+ */ 
 void uart_puts(const char *s);
 
-//Send file
+/** @brief Send file.
+ *
+ * Send variable size file via UART.
+ * Order of commands:
+ *  1. Send file transmit (FTX) comnand.
+ *  2. Send file_name.
+ *  3. Send file_size (in little endian format).
+ *  4. Send file.
+ *
+ * @param file_name Pointer to file name string.
+ * @param file_size Size of file to be sent.
+ * @param mem Pointer to file.
+ * @return void.
+ */ 
 void uart_sendfile(char* file_name, int file_size, char *mem);
 
-//RX FUNCTIONS
 
-//Wait for rx to be ready
+/** @brief Wait for RX Data.
+ *
+ * Active wait for RX incomming data.
+ *
+ * @return void.
+ */ 
 void uart_rxwait();
 
-//Get char
+/** @brief Get char.
+ *
+ * Active wait and receive char/byte from UART.
+ *
+ * @return received byte from UART.
+ */ 
 char uart_getc();
 
-//Receive file 
+/** @brief Receive file.
+ *
+ * Request variable size file via UART.
+ * Order of commands:
+ *  1. Send file receive (FRX) command.
+ *  2. Send file_name.
+ *  3. Receive file_size (in little endian format).
+ *  4. Send ACK command.
+ *  5. Receive file.
+ *
+ * If memory pointer is not inicialized, allocates memory for incomming file.
+ *
+ * @param file_name Pointer to file name string.
+ * @param mem Pointer in memory to store incomming file.
+ * @return Size of received file.
+ */ 
 int uart_recvfile(char* file_name, char **mem);
