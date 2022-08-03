3962c8e5e9e4c2d7a0261a879c455fc14957cb4d
PMiranda <pmiranda.iobundle@gmail.com>
2022-08-03 12:40:33 +0100

    feat: remove document and fpga files - remove all files related to documentation and fpga - update Makefile to remove documentation and fpga targets - update config.mk - update LIB with merge from build-lib and master branches

--- config.mk
+++ config.mk
@@ -4,24 +4,13 @@ TOP_MODULE=iob_uart
 
 #PATHS
 REMOTE_ROOT_DIR ?=sandbox/iob-uart
-SIM_DIR ?=$(UART_HW_DIR)/simulation
-FPGA_DIR ?=$(shell find $(UART_DIR)/hardware -name $(FPGA_FAMILY))
-DOC_DIR ?=$(UART_DIR)/document/$(DOC)
 
-LIB_DIR ?=$(UART_DIR)/submodules/LIB
 UART_HW_DIR:=$(UART_DIR)/hardware
+SIM_DIR ?=$(UART_HW_DIR)/simulation
+LIB_DIR ?=$(UART_DIR)/submodules/LIB
 
 #MAKE SW ACCESSIBLE REGISTER
 MKREGS:=$(shell find $(LIB_DIR) -name mkregs.py)
-
-#DEFAULT FPGA FAMILY AND FAMILY LIST
-FPGA_FAMILY ?=CYCLONEV-GT
-FPGA_FAMILY_LIST ?=CYCLONEV-GT XCKU
-
-#DEFAULT DOC AND doc LIST
-DOC ?=pb
-DOC_LIST ?=pb ug
-
 
 # default target
 default: sim
