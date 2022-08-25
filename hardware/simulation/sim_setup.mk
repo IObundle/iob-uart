07033d478f57f7f9606273be0bbf9e8742f3b61a
PMiranda <pmiranda.iobundle@gmail.com>
2022-08-25 15:07:41 +0100

    fix(sim): move testbench to BUILD_DIR/hw/sim - update LIB submodule - move setup variables to `config_setup.mk` - copy `uart_tb.v` to `BUILD_DIR/hw/sim`

--- hardware/simulation/sim_setup.mk
+++ hardware/simulation/sim_setup.mk
@@ -3,6 +3,6 @@
 #
 
 # copy simulation wrapper
-VSRC+=$(BUILD_VSRC_DIR)/uart_tb.v
-$(BUILD_VSRC_DIR)/uart_tb.v: $(CORE_SIM_DIR)/uart_tb.v
-	cp $< $(BUILD_VSRC_DIR)
+VSRC+=$(BUILD_SIM_DIR)/uart_tb.v
+$(BUILD_SIM_DIR)/uart_tb.v: $(CORE_SIM_DIR)/uart_tb.v
+	cp $< $(BUILD_SIM_DIR)
