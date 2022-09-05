c6cff299e620eb78200284a4208bb5ef4ffc2703
Jose T. de Sousa <jose.t.de.sousa@gmail.com>
2022-09-05 23:30:16 +0100

    delete unused file, update lib

--- hardware/simulation/sim_setup.mk
+++
@@ -1,8 +0,0 @@
-#
-# This file segment is included in LIB_DIR/Makefile
-#
-
-# copy simulation wrapper
-SRC+=$(BUILD_SIM_DIR)/uart_tb.v
-$(BUILD_SIM_DIR)/uart_tb.v: $(CORE_SIM_DIR)/uart_tb.v
-	cp $< $(BUILD_SIM_DIR)
