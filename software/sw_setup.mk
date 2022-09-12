31921c8d283c11ecd6fd1aa673a292eb1117cf39
PMiranda <pmiranda.iobundle@gmail.com>
2022-09-12 11:13:07 +0100

    fix(LIB): update to latest LIB - update LIB submodule - remove unsupported `config.mk` - rename `info.mk` to `config_setup.mk` - fix removed makefile variables during setup

--- software/sw_setup.mk
+++ software/sw_setup.mk
@@ -7,26 +7,26 @@
 #
 
 #sources
-SRC+=$(subst $(UART_DIR)/software, $(BUILD_SW_SRC_DIR), $(wildcard $(UART_DIR)/software/*.c))
-$(BUILD_SW_SRC_DIR)/%.c: $(UART_DIR)/software/%.c
+SRC+=$(subst $(UART_DIR)/software, $(BUILD_DIR)/sw/src, $(wildcard $(UART_DIR)/software/*.c))
+$(BUILD_DIR)/sw/src/%.c: $(UART_DIR)/software/%.c
 	cp $< $@
 
-SRC+=$(subst $(UART_DIR)/software, $(BUILD_SW_SRC_DIR), $(wildcard $(UART_DIR)/software/*.h))
-$(BUILD_SW_SRC_DIR)/%.h: $(UART_DIR)/software/%.h
+SRC+=$(subst $(UART_DIR)/software, $(BUILD_DIR)/sw/src, $(wildcard $(UART_DIR)/software/*.h))
+$(BUILD_DIR)/sw/src/%.h: $(UART_DIR)/software/%.h
 	cp $< $@
 
 #sw accessible register headers
-SRC+=$(BUILD_SW_SRC_DIR)/iob_uart_swreg.h $(BUILD_SW_SRC_DIR)/iob_uart_swreg_emb.c
-$(BUILD_SW_SRC_DIR)/iob_uart_swreg.h: iob_uart_swreg.h
+SRC+=$(BUILD_DIR)/sw/src/iob_uart_swreg.h $(BUILD_DIR)/sw/src/iob_uart_swreg_emb.c
+$(BUILD_DIR)/sw/src/iob_uart_swreg.h: iob_uart_swreg.h
 	mv $< $@
 
-$(BUILD_SW_SRC_DIR)/iob_uart_swreg_emb.c: iob_uart_swreg_emb.c
+$(BUILD_DIR)/sw/src/iob_uart_swreg_emb.c: iob_uart_swreg_emb.c
 	mv $< $@
 
 iob_uart_swreg.h iob_uart_swreg_emb.c: $(UART_DIR)/mkregs.conf
 	$(LIB_DIR)/software/python/mkregs.py iob_uart $(UART_DIR) SW
 
 # PC emul sources
-SRC+=$(BUILD_SW_PCSRC_DIR)/iob_uart_swreg_pc_emul.c
-$(BUILD_SW_PCSRC_DIR)/iob_uart_swreg_pc_emul.c: $(UART_DIR)/software/pc-emul/iob_uart_swreg_pc_emul.c
+SRC+=$(BUILD_DIR)/sw/pcsrc/iob_uart_swreg_pc_emul.c
+$(BUILD_DIR)/sw/pcsrc/iob_uart_swreg_pc_emul.c: $(UART_DIR)/software/pc-emul/iob_uart_swreg_pc_emul.c
 	cp $< $@
