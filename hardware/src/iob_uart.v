6654e00d5e43e6161c3cd23273905fe0364de0e8
PMiranda <pmiranda.iobundle@gmail.com>
2022-10-13 11:37:12 +0100

    feat(LIB): update LIB submodule - update LIB submodule - update gen_if to clkrst_port - update clk and rst wires accordingly - update iob_s_if to use `addr` instead of `address` - replace deprecated iob_lib macros

--- hardware/src/iob_uart.v
+++ hardware/src/iob_uart.v
@@ -21,7 +21,7 @@ module iob_uart
    `IOB_INPUT(rxd, 1), //Serial receive line
    `IOB_INPUT(cts, 1), //Clear to send; the destination is ready to receive a transmission sent by the UART
    `IOB_OUTPUT(rts, 1), //Ready to send; the UART is ready to receive a transmission from the sender.
-`include "iob_gen_if.vh"
+`include "iob_clkrst_port.vh"
    );
 
 //BLOCK Register File & Configuration control and status register file.
@@ -32,9 +32,9 @@ module iob_uart
     `IOB_WIRE(UART_SOFTRESET, 1)
     iob_reg #(.DATA_W(1), .RST_VAL(0))
     uart_softreset (
-        .clk        (clk),
-        .arst       (rst),
-        .rst        (rst),
+        .clk        (clk_i),
+        .arst       (rst_i),
+        .rst        (rst_i),
         .en         (UART_SOFTRESET_en),
         .data_in    (UART_SOFTRESET_wdata[0]),
         .data_out   (UART_SOFTRESET)
@@ -43,9 +43,9 @@ module iob_uart
     `IOB_WIRE(UART_DIV, 16)
     iob_reg #(.DATA_W(16), .RST_VAL(0))
     uart_div (
-        .clk        (clk),
-        .arst       (rst),
-        .rst        (rst),
+        .clk        (clk_i),
+        .arst       (rst_i),
+        .rst        (rst_i),
         .en         (UART_DIV_en),
         .data_in    (UART_DIV_wdata),
         .data_out   (UART_DIV)
@@ -54,9 +54,9 @@ module iob_uart
     `IOB_WIRE(UART_TXDATA, 8)
     iob_reg #(.DATA_W(8), .RST_VAL(0))
     uart_txdata (
-        .clk        (clk),
-        .arst       (rst),
-        .rst        (rst),
+        .clk        (clk_i),
+        .arst       (rst_i),
+        .rst        (rst_i),
         .en         (UART_TXDATA_en),
         .data_in    (UART_TXDATA_wdata),
         .data_out   (UART_TXDATA)
@@ -65,9 +65,9 @@ module iob_uart
     `IOB_WIRE(UART_TXEN, 1)
     iob_reg #(.DATA_W(1), .RST_VAL(0))
     uart_txen (
-        .clk        (clk),
-        .arst       (rst),
-        .rst        (rst),
+        .clk        (clk_i),
+        .arst       (rst_i),
+        .rst        (rst_i),
         .en         (UART_TXEN_en),
         .data_in    (UART_TXEN_wdata[0]),
         .data_out   (UART_TXEN)
@@ -76,21 +76,21 @@ module iob_uart
     `IOB_WIRE(UART_RXEN, 1)
     iob_reg #(.DATA_W(1), .RST_VAL(0))
     uart_rxen (
-        .clk        (clk),
-        .arst       (rst),
-        .rst        (rst),
+        .clk        (clk_i),
+        .arst       (rst_i),
+        .rst        (rst_i),
         .en         (UART_RXEN_en),
         .data_in    (UART_RXEN_wdata[0]),
         .data_out   (UART_RXEN)
     );
 
-    `IOB_WIRE2WIRE({(`UART_TXREADY_W-1){1'b0}}, UART_TXREADY_rdata[`UART_TXREADY_W-1:1])
-    `IOB_WIRE2WIRE({(`UART_RXREADY_W-1){1'b0}}, UART_RXREADY_rdata[`UART_RXREADY_W-1:1])
+    assign UART_TXREADY_rdata[`UART_TXREADY_W-1:1] = {(`UART_TXREADY_W-1){1'b0}};
+    assign UART_RXREADY_rdata[`UART_RXREADY_W-1:1] = {(`UART_RXREADY_W-1){1'b0}};
 
    uart_core uart_core0 
      (
-      .clk(clk),
-      .rst(rst),
+      .clk(clk_i),
+      .rst(rst_i),
       .rst_soft(UART_SOFTRESET),
       .tx_en(UART_TXEN),
       .rx_en(UART_RXEN),
@@ -99,7 +99,7 @@ module iob_uart
       .tx_data(UART_TXDATA),
       .rx_data(UART_RXDATA_rdata),
       .data_write_en(UART_TXDATA_en),
-      .data_read_en(valid & !wstrb & (address == (`UART_RXDATA_ADDR >> 2))),
+      .data_read_en(valid & !wstrb & (addr == (`UART_RXDATA_ADDR >> 2))),
       .bit_duration(UART_DIV),
       .rxd(rxd),
       .txd(txd),
