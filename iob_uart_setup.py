cb16c3d9f692af752e4cc3d627c5494856cf6dcd
Pedro Antunes <pedronmantunes@gmail.com>
2022-11-22 03:03:50 +0000

    [*_setup.py] Fix: there are no RW registers;

--- iob_uart_setup.py
+++ iob_uart_setup.py
@@ -17,14 +17,14 @@ ios = \
 regs = \
 [
     {'name': 'uart', 'descr':'UART software accessible registers.', 'regs': [
-        {'name':"DIV", 'type':"RW", 'n_bits':16, 'rst_val':0, 'addr':-1, 'n_items':1, 'autologic':True, 'descr':"Bit duration in system clock cycles."},
+        {'name':"DIV", 'type':"W", 'n_bits':16, 'rst_val':0, 'addr':-1, 'n_items':1, 'autologic':True, 'descr':"Bit duration in system clock cycles."},
         {'name':"SOFTRESET", 'type':"W", 'n_bits':1, 'rst_val':0, 'addr':-1, 'n_items':1, 'autologic':True, 'descr':"Soft reset."},
-        {'name':"TXDATA", 'type':"RW", 'n_bits':8, 'rst_val':0, 'addr':-1, 'n_items':1, 'autologic':True, 'descr':"TX data."},
-        {'name':"TXEN", 'type':"RW", 'n_bits':1, 'rst_val':0, 'addr':-1, 'n_items':1, 'autologic':True, 'descr':"TX enable."},
-        {'name':"TXREADY", 'type':"RW", 'n_bits':1, 'rst_val':0, 'addr':-1, 'n_items':1, 'autologic':True, 'descr':"TX ready to receive data."},
-        {'name':"RXDATA", 'type':"RW", 'n_bits':8, 'rst_val':0, 'addr':-1, 'n_items':1, 'autologic':True, 'descr':"RX data."},
-        {'name':"RXEN", 'type':"RW", 'n_bits':1, 'rst_val':0, 'addr':-1, 'n_items':1, 'autologic':True, 'descr':"RX enable."},
-        {'name':"RXREADY", 'type':"RW", 'n_bits':1, 'rst_val':0, 'addr':-1, 'n_items':1, 'autologic':True, 'descr':"RX data is ready to be read."}
+        {'name':"TXDATA", 'type':"W", 'n_bits':8, 'rst_val':0, 'addr':-1, 'n_items':1, 'autologic':True, 'descr':"TX data."},
+        {'name':"TXEN", 'type':"W", 'n_bits':1, 'rst_val':0, 'addr':-1, 'n_items':1, 'autologic':True, 'descr':"TX enable."},
+        {'name':"TXREADY", 'type':"R", 'n_bits':1, 'rst_val':0, 'addr':-1, 'n_items':1, 'autologic':True, 'descr':"TX ready to receive data."},
+        {'name':"RXDATA", 'type':"R", 'n_bits':8, 'rst_val':0, 'addr':-1, 'n_items':1, 'autologic':True, 'descr':"RX data."},
+        {'name':"RXEN", 'type':"W", 'n_bits':1, 'rst_val':0, 'addr':-1, 'n_items':1, 'autologic':True, 'descr':"RX enable."},
+        {'name':"RXREADY", 'type':"R", 'n_bits':1, 'rst_val':0, 'addr':-1, 'n_items':1, 'autologic':True, 'descr':"RX data is ready to be read."}
     ]}
 ]
 
