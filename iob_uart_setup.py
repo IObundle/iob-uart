c2213e22606889bbab3b2fe4d379a5cccaa0e579
Pedro Antunes <pedronmantunes@gmail.com>
2022-11-22 17:48:23 +0000

    [iob-lib] Update LIB submodule;

--- iob_uart_setup.py
+++ iob_uart_setup.py
@@ -17,8 +17,8 @@ ios = \
 regs = \
 [
     {'name': 'uart', 'descr':'UART software accessible registers.', 'regs': [
-        {'name':"DIV", 'type':"W", 'n_bits':16, 'rst_val':0, 'addr':-1, 'n_items':1, 'autologic':True, 'descr':"Bit duration in system clock cycles."},
         {'name':"SOFTRESET", 'type':"W", 'n_bits':1, 'rst_val':0, 'addr':-1, 'n_items':1, 'autologic':True, 'descr':"Soft reset."},
+        {'name':"DIV", 'type':"W", 'n_bits':16, 'rst_val':0, 'addr':-1, 'n_items':1, 'autologic':True, 'descr':"Bit duration in system clock cycles."},
         {'name':"TXDATA", 'type':"W", 'n_bits':8, 'rst_val':0, 'addr':-1, 'n_items':1, 'autologic':True, 'descr':"TX data."},
         {'name':"TXEN", 'type':"W", 'n_bits':1, 'rst_val':0, 'addr':-1, 'n_items':1, 'autologic':True, 'descr':"TX enable."},
         {'name':"TXREADY", 'type':"R", 'n_bits':1, 'rst_val':0, 'addr':-1, 'n_items':1, 'autologic':True, 'descr':"TX ready to receive data."},
