83dcfa9c4419cf27f9896ac82f67f5ef96b392c0
Artur Nóbrega <arturnobrega1999@gmail.com>
2022-12-07 18:09:42 +0000

    feat(params): add values to params and remove associated macros

--- iob_uart_setup.py
+++ iob_uart_setup.py
@@ -10,13 +10,11 @@ version = 'V0.10'
 confs = \
 [
     # Macros
-    {'name':'DATA_W', 'type':'M', 'val':'32', 'min':'NA', 'max':'NA', 'descr':"Data bus width"},
-    {'name':'ADDR_W', 'type':'M', 'val':'`IOB_UART_SWREG_ADDR_W', 'min':'NA', 'max':'NA', 'descr':"Address bus width"},
-    {'name':'UART_DATA_W', 'type':'M', 'val':'8', 'min':'NA', 'max':'NA', 'descr':""},
+
     # Parameters
-    {'name':'DATA_W', 'type':'P', 'descr':"Data bus width"},
-    {'name':'ADDR_W', 'type':'P', 'descr':"Address bus width"},
-    {'name':'UART_DATA_W', 'type':'P', 'descr':""}
+    {'name':'DATA_W',      'type':'P', 'val':'32', 'min':'NA', 'max':'NA', 'descr':"Data bus width"},
+    {'name':'ADDR_W',      'type':'P', 'val':'`IOB_UART_SWREG_ADDR_W', 'min':'NA', 'max':'NA', 'descr':"Address bus width"},
+    {'name':'UART_DATA_W', 'type':'P', 'val':'8', 'min':'NA', 'max':'NA', 'descr':""},
 ]
 
 ios = \
