afffed47fadeb93bc5b4320bf93a0e24da0fd4bc
Pedro Antunes <pedronmantunes@gmail.com>
2022-12-09 23:04:47 +0000

    Automatic generated registers were correct;

--- iob_uart_setup.py
+++ iob_uart_setup.py
@@ -42,14 +42,14 @@ ios = \
 regs = \
 [
     {'name': 'uart', 'descr':'UART software accessible registers.', 'regs': [
-        {'name':"SOFTRESET", 'type':"W", 'n_bits':1, 'rst_val':0, 'addr':0, 'n_items':1, 'autologic':True, 'descr':"Soft reset."},
-        {'name':"DIV", 'type':"W", 'n_bits':16, 'rst_val':0, 'addr':2, 'n_items':1, 'autologic':True, 'descr':"Bit duration in system clock cycles."},
-        {'name':"TXDATA", 'type':"W", 'n_bits':'UART_DATA_W', 'rst_val':0, 'addr':4, 'n_items':1, 'autologic':True, 'descr':"TX data."},
-        {'name':"TXEN", 'type':"W", 'n_bits':1, 'rst_val':0, 'addr':5, 'n_items':1, 'autologic':True, 'descr':"TX enable."},
-        {'name':"TXREADY", 'type':"R", 'n_bits':1, 'rst_val':0, 'addr':6, 'n_items':1, 'autologic':True, 'descr':"TX ready to receive data."},
-        {'name':"RXDATA", 'type':"R", 'n_bits':'UART_DATA_W', 'rst_val':0, 'addr':8, 'n_items':1, 'autologic':True, 'descr':"RX data."},
-        {'name':"RXEN", 'type':"W", 'n_bits':1, 'rst_val':0, 'addr':10, 'n_items':1, 'autologic':True, 'descr':"RX enable."},
-        {'name':"RXREADY", 'type':"R", 'n_bits':1, 'rst_val':0, 'addr':11, 'n_items':1, 'autologic':True, 'descr':"RX data is ready to be read."}
+        {'name':"SOFTRESET", 'type':"W", 'n_bits':1, 'rst_val':0, 'addr':-1, 'n_items':1, 'autologic':True, 'descr':"Soft reset."},
+        {'name':"DIV", 'type':"W", 'n_bits':16, 'rst_val':0, 'addr':-1, 'n_items':1, 'autologic':True, 'descr':"Bit duration in system clock cycles."},
+        {'name':"TXDATA", 'type':"W", 'n_bits':'UART_DATA_W', 'rst_val':0, 'addr':-1, 'n_items':1, 'autologic':True, 'descr':"TX data."},
+        {'name':"TXEN", 'type':"W", 'n_bits':1, 'rst_val':0, 'addr':-1, 'n_items':1, 'autologic':True, 'descr':"TX enable."},
+        {'name':"TXREADY", 'type':"R", 'n_bits':1, 'rst_val':0, 'addr':-1, 'n_items':1, 'autologic':True, 'descr':"TX ready to receive data."},
+        {'name':"RXDATA", 'type':"R", 'n_bits':'UART_DATA_W', 'rst_val':0, 'addr':-1, 'n_items':1, 'autologic':True, 'descr':"RX data."},
+        {'name':"RXEN", 'type':"W", 'n_bits':1, 'rst_val':0, 'addr':-1, 'n_items':1, 'autologic':True, 'descr':"RX enable."},
+        {'name':"RXREADY", 'type':"R", 'n_bits':1, 'rst_val':0, 'addr':-1, 'n_items':1, 'autologic':True, 'descr':"RX data is ready to be read."}
     ]}
 ]
 
