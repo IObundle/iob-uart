603eb208d0bf92500d5ecae1c2b63d1670f2783e
Artur Nóbrega <arturnobrega1999@gmail.com>
2022-12-12 20:19:49 +0000

    feat(uart): fix write and read enable signals - Manually configured RXDATA address to ensure it has a full address   allocated for it. This allows checking when the CPU performs a read   operation on it. If multiple registers where in this address, we would   be unable to check which register the CPU is reading from. - Disabled Auto-Logic for RXDATA and TXDATA registers. This allows for   access of the respective RXDATA_ren and TXDATA_wen signals in the UART   logic.

--- hardware/src/iob_uart.v
+++ hardware/src/iob_uart.v
@@ -12,6 +12,14 @@ module iob_uart # (
    //BLOCK Register File & Configuration control and status register file.
    `include "iob_uart_swreg_inst.vh"
 
+   // TXDATA Manual logic
+    wire [UART_DATA_W-1:0] TXDATA_o;
+   iob_reg_ae #(UART_DATA_W,0) TXDATA_datareg (clk_i, arst_i, TXDATA_wen, TXDATA, TXDATA_o);
+   assign TXDATA_ready = 1'b1;
+   // RXDATA Manual logic
+   assign RXDATA_ready = 1'b1;
+   assign RXDATA_rvalid = 1'b1;
+
    uart_core uart_core0 
      (
       .clk_i(clk_i),
@@ -21,10 +29,10 @@ module iob_uart # (
       .rx_en_i(RXEN),
       .tx_ready_o(TXREADY),
       .rx_ready_o(RXREADY),
-      .tx_data_i(TXDATA),
+      .tx_data_i(TXDATA_o),
       .rx_data_o(RXDATA),
-      .data_write_en_i(iob_avalid & (| iob_wstrb) & (iob_addr == (`IOB_UART_TXDATA_ADDR))),
-      .data_read_en_i(iob_avalid & !iob_wstrb & (iob_addr == (`IOB_UART_RXDATA_ADDR))),
+      .data_write_en_i(TXDATA_wen),
+      .data_read_en_i(RXDATA_ren),
       .bit_duration_i(DIV),
       .rxd_i(rxd),
       .txd_o(txd),
