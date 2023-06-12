05a1289f855612cafe7b0ee141d351e58906d72e
Artur Nóbrega <arturnobrega1999@gmail.com>
2023-06-12 18:17:41 +0100

    feat(LIB): Add missing lib modules; Format code; Update LIB.

--- iob_uart.py
+++ iob_uart.py
@@ -7,20 +7,29 @@ from iob_module import iob_module
 from setup import setup
 
 # Submodules
+from iob_lib import iob_lib
+from iob_utils import iob_utils
+from iob_clkenrst_portmap import iob_clkenrst_portmap
+from iob_clkenrst_port import iob_clkenrst_port
 from iob_reg import iob_reg
 from iob_reg_e import iob_reg_e
 
+
 class iob_uart(iob_module):
-    name='iob_uart'
-    version="V0.10"
-    flows="sim emb doc"
-    setup_dir=os.path.dirname(__file__)
+    name = "iob_uart"
+    version = "V0.10"
+    flows = "sim emb doc"
+    setup_dir = os.path.dirname(__file__)
 
     @classmethod
     def _run_setup(cls):
         # Hardware headers & modules
         iob_module.generate("iob_s_port")
         iob_module.generate("iob_s_portmap")
+        iob_lib.setup()
+        iob_utils.setup()
+        iob_clkenrst_portmap.setup()
+        iob_clkenrst_port.setup()
         iob_reg.setup()
         iob_reg_e.setup()
 
@@ -38,50 +47,189 @@ class iob_uart(iob_module):
         # Setup core using LIB function
         setup(cls)
 
-
     @classmethod
     def _setup_confs(cls):
-        super()._setup_confs([
-            # Macros
-
-            # Parameters
-            {'name':'DATA_W',      'type':'P', 'val':'32', 'min':'NA', 'max':'NA', 'descr':"Data bus width"},
-            {'name':'ADDR_W',      'type':'P', 'val':'`IOB_UART_SWREG_ADDR_W', 'min':'NA', 'max':'NA', 'descr':"Address bus width"},
-            {'name':'UART_DATA_W', 'type':'P', 'val':'8', 'min':'NA', 'max':'8', 'descr':""}
-        ])
+        super()._setup_confs(
+            [
+                # Macros
+                # Parameters
+                {
+                    "name": "DATA_W",
+                    "type": "P",
+                    "val": "32",
+                    "min": "NA",
+                    "max": "NA",
+                    "descr": "Data bus width",
+                },
+                {
+                    "name": "ADDR_W",
+                    "type": "P",
+                    "val": "`IOB_UART_SWREG_ADDR_W",
+                    "min": "NA",
+                    "max": "NA",
+                    "descr": "Address bus width",
+                },
+                {
+                    "name": "UART_DATA_W",
+                    "type": "P",
+                    "val": "8",
+                    "min": "NA",
+                    "max": "8",
+                    "descr": "",
+                },
+            ]
+        )
 
     @classmethod
     def _setup_ios(cls):
         cls.ios += [
-            {'name': 'iob_s_port', 'descr':'CPU native interface', 'ports': [
-            ]},
-            {'name': 'general', 'descr':'GENERAL INTERFACE SIGNALS', 'ports': [
-                {'name':"clk_i" , 'type':"I", 'n_bits':'1', 'descr':"System clock input"},
-                {'name':"arst_i", 'type':"I", 'n_bits':'1', 'descr':"System reset, asynchronous and active high"},
-                {'name':"cke_i" , 'type':"I", 'n_bits':'1', 'descr':"System reset, asynchronous and active high"}
-            ]},
-            {'name': 'rs232', 'descr':'Cache invalidate and write-trough buffer IO chain', 'ports': [
-                #{'name':'interrupt', 'type':'O', 'n_bits':'1', 'descr':'be done'},
-                {'name':'txd', 'type':'O', 'n_bits':'1', 'descr':'transmit line'},
-                {'name':'rxd', 'type':'I', 'n_bits':'1', 'descr':'receive line'},
-                {'name':'cts', 'type':'I', 'n_bits':'1', 'descr':'to send; the destination is ready to receive a transmission sent by the UART'},
-                {'name':'rts', 'type':'O', 'n_bits':'1', 'descr':'to send; the UART is ready to receive a transmission from the sender.'}
-            ]}
+            {"name": "iob_s_port", "descr": "CPU native interface", "ports": []},
+            {
+                "name": "general",
+                "descr": "GENERAL INTERFACE SIGNALS",
+                "ports": [
+                    {
+                        "name": "clk_i",
+                        "type": "I",
+                        "n_bits": "1",
+                        "descr": "System clock input",
+                    },
+                    {
+                        "name": "arst_i",
+                        "type": "I",
+                        "n_bits": "1",
+                        "descr": "System reset, asynchronous and active high",
+                    },
+                    {
+                        "name": "cke_i",
+                        "type": "I",
+                        "n_bits": "1",
+                        "descr": "System reset, asynchronous and active high",
+                    },
+                ],
+            },
+            {
+                "name": "rs232",
+                "descr": "Cache invalidate and write-trough buffer IO chain",
+                "ports": [
+                    # {'name':'interrupt', 'type':'O', 'n_bits':'1', 'descr':'be done'},
+                    {
+                        "name": "txd",
+                        "type": "O",
+                        "n_bits": "1",
+                        "descr": "transmit line",
+                    },
+                    {
+                        "name": "rxd",
+                        "type": "I",
+                        "n_bits": "1",
+                        "descr": "receive line",
+                    },
+                    {
+                        "name": "cts",
+                        "type": "I",
+                        "n_bits": "1",
+                        "descr": "to send; the destination is ready to receive a transmission sent by the UART",
+                    },
+                    {
+                        "name": "rts",
+                        "type": "O",
+                        "n_bits": "1",
+                        "descr": "to send; the UART is ready to receive a transmission from the sender.",
+                    },
+                ],
+            },
         ]
 
     @classmethod
     def _setup_regs(cls):
         cls.regs += [
-            {'name': 'uart', 'descr':'UART software accessible registers.', 'regs': [
-                {'name':"SOFTRESET", 'type':"W", 'n_bits':1, 'rst_val':0, 'addr':-1, 'log2n_items':0, 'autologic':True, 'descr':"Soft reset."},
-                {'name':"DIV", 'type':"W", 'n_bits':16, 'rst_val':0, 'addr':-1, 'log2n_items':0, 'autologic':True, 'descr':"Bit duration in system clock cycles."},
-                {'name':"TXDATA", 'type':"W", 'n_bits':'UART_DATA_W', 'rst_val':0, 'addr':-1, 'log2n_items':0, 'autologic':False, 'descr':"TX data."},
-                {'name':"TXEN", 'type':"W", 'n_bits':1, 'rst_val':0, 'addr':-1, 'log2n_items':0, 'autologic':True, 'descr':"TX enable."},
-                {'name':"RXEN", 'type':"W", 'n_bits':1, 'rst_val':0, 'addr':6, 'log2n_items':0, 'autologic':True, 'descr':"RX enable."},
-                {'name':"TXREADY", 'type':"R", 'n_bits':1, 'rst_val':0, 'addr':-1, 'log2n_items':0, 'autologic':True, 'descr':"TX ready to receive data."},
-                {'name':"RXREADY", 'type':"R", 'n_bits':1, 'rst_val':0, 'addr':-1, 'log2n_items':0, 'autologic':True, 'descr':"RX data is ready to be read."},
-                {'name':"RXDATA", 'type':"R", 'n_bits':'UART_DATA_W', 'rst_val':0, 'addr':4, 'log2n_items':0, 'autologic':False, 'descr':"RX data."},
-            ]}
+            {
+                "name": "uart",
+                "descr": "UART software accessible registers.",
+                "regs": [
+                    {
+                        "name": "SOFTRESET",
+                        "type": "W",
+                        "n_bits": 1,
+                        "rst_val": 0,
+                        "addr": -1,
+                        "log2n_items": 0,
+                        "autologic": True,
+                        "descr": "Soft reset.",
+                    },
+                    {
+                        "name": "DIV",
+                        "type": "W",
+                        "n_bits": 16,
+                        "rst_val": 0,
+                        "addr": -1,
+                        "log2n_items": 0,
+                        "autologic": True,
+                        "descr": "Bit duration in system clock cycles.",
+                    },
+                    {
+                        "name": "TXDATA",
+                        "type": "W",
+                        "n_bits": "UART_DATA_W",
+                        "rst_val": 0,
+                        "addr": -1,
+                        "log2n_items": 0,
+                        "autologic": False,
+                        "descr": "TX data.",
+                    },
+                    {
+                        "name": "TXEN",
+                        "type": "W",
+                        "n_bits": 1,
+                        "rst_val": 0,
+                        "addr": -1,
+                        "log2n_items": 0,
+                        "autologic": True,
+                        "descr": "TX enable.",
+                    },
+                    {
+                        "name": "RXEN",
+                        "type": "W",
+                        "n_bits": 1,
+                        "rst_val": 0,
+                        "addr": 6,
+                        "log2n_items": 0,
+                        "autologic": True,
+                        "descr": "RX enable.",
+                    },
+                    {
+                        "name": "TXREADY",
+                        "type": "R",
+                        "n_bits": 1,
+                        "rst_val": 0,
+                        "addr": -1,
+                        "log2n_items": 0,
+                        "autologic": True,
+                        "descr": "TX ready to receive data.",
+                    },
+                    {
+                        "name": "RXREADY",
+                        "type": "R",
+                        "n_bits": 1,
+                        "rst_val": 0,
+                        "addr": -1,
+                        "log2n_items": 0,
+                        "autologic": True,
+                        "descr": "RX data is ready to be read.",
+                    },
+                    {
+                        "name": "RXDATA",
+                        "type": "R",
+                        "n_bits": "UART_DATA_W",
+                        "rst_val": 0,
+                        "addr": 4,
+                        "log2n_items": 0,
+                        "autologic": False,
+                        "descr": "RX data.",
+                    },
+                ],
+            }
         ]
 
     @classmethod
