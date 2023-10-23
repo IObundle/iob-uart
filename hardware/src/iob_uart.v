64e6d4ebef87297d81ea4de4e96e10d40b6f3abe
Artur Nóbrega <arturnobrega1999@gmail.com>
2023-10-23 23:45:37 +0100

    fix(regs): Add `_w` and `_r` suffix to register connections

--- hardware/src/iob_uart.v
+++ hardware/src/iob_uart.v
@@ -16,7 +16,7 @@ module iob_uart #(
    `include "iob_uart_swreg_inst.vs"
 
    // TXDATA Manual logic
-   wire [UART_DATA_W-1:0] TXDATA = iob_wdata_i[8*(`IOB_UART_TXDATA_ADDR%(DATA_W/8))+:UART_DATA_W];
+   wire [UART_DATA_W-1:0] TXDATA_w = iob_wdata_i[8*(`IOB_UART_TXDATA_ADDR%(DATA_W/8))+:UART_DATA_W];
    assign TXDATA_ready  = 1'b1;
    
    // RXDATA Manual logic
@@ -26,16 +26,16 @@ module iob_uart #(
    uart_core uart_core0 (
       .clk_i          (clk_i),
       .rst_i          (arst_i),
-      .rst_soft_i     (SOFTRESET),
-      .tx_en_i        (TXEN),
-      .rx_en_i        (RXEN),
-      .tx_ready_o     (TXREADY),
-      .rx_ready_o     (RXREADY),
-      .tx_data_i      (TXDATA),
-      .rx_data_o      (RXDATA),
+      .rst_soft_i     (SOFTRESET_w),
+      .tx_en_i        (TXEN_w),
+      .rx_en_i        (RXEN_w),
+      .tx_ready_o     (TXREADY_r),
+      .rx_ready_o     (RXREADY_r),
+      .tx_data_i      (TXDATA_w),
+      .rx_data_o      (RXDATA_r),
       .data_write_en_i(TXDATA_wen),
       .data_read_en_i (RXDATA_ren),
-      .bit_duration_i (DIV),
+      .bit_duration_i (DIV_w),
       .rxd_i          (rxd_i),
       .txd_o          (txd_o),
       .cts_i          (cts_i),
